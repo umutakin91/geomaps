a43210123dfe0a15edab3e9a62f8baa50f25ed05
Umut AKIN <umutakin91@gmail.com>
2019-10-01 18:10:11 +0200

    Initial commit for spring boot setup.

---
+++ src/main/java/com/example/broadcom/geoserver/GeoserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.broadcom.geoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoserverApplication.class, args);
+	}
+
+}
