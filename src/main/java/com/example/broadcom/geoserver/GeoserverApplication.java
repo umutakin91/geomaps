5f3f4bec4180f814cc43254234231c25aace0e28
Umut AKIN <umutakin91@gmail.com>
2019-10-01 18:54:04 +0200

    WIP #1; first checkstyle errors fixed.

--- src/main/java/com/example/broadcom/geoserver/GeoserverApplication.java
+++ src/main/java/com/example/broadcom/geoserver/GeoserverApplication.java
@@ -3,11 +3,16 @@ package com.example.broadcom.geoserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Spring Boot Application starter class.
+ *
+ * @author Umut AKIN
+ */
 @SpringBootApplication
 public class GeoserverApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GeoserverApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GeoserverApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/broadcom/geoserver/GeoserverApplicationTests.java
+++ src/test/java/com/example/broadcom/geoserver/GeoserverApplicationTests.java
@@ -5,12 +5,17 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * Spring Boot main test class
+ *
+ * @author Umut AKIN
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GeoserverApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
