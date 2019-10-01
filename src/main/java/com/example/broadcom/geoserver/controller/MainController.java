c839ad3c5ee83876449c4ce3ae46dec3891dbf9b
Umut AKIN <umutakin91@gmail.com>
2019-10-01 20:00:06 +0200

    WIP #3; main controller and welcome page added.

---
+++ src/main/java/com/example/broadcom/geoserver/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.example.broadcom.geoserver.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String main(Model model) {
+
+        return "welcome"; //view
+    }
+
+}
