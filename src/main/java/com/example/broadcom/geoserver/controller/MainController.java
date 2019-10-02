f6d1be4e8316c1fce75a749cd0651658b0627ee7
Umut AKIN <umutakin91@gmail.com>
2019-10-02 19:22:45 +0200

    WIP #3; search input introduced to model of controller.

--- src/main/java/com/example/broadcom/geoserver/controller/MainController.java
+++ src/main/java/com/example/broadcom/geoserver/controller/MainController.java
@@ -4,12 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.broadcom.geoserver.dto.SearchQueryDto;
+
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String main(Model model) {
+    public String main(Model model,SearchQueryDto searchInput) {
 
+        model.addAttribute("searchInput" , searchInput);
         return "welcome"; //view
     }
 
