1d08d2752f9c35503867700b650f5f12b4e6c83c
Umut AKIN <umutakin91@gmail.com>
2019-10-02 19:24:38 +0200

    WIP #3; Post mapping and search result page introduced.

--- src/main/java/com/example/broadcom/geoserver/controller/MainController.java
+++ src/main/java/com/example/broadcom/geoserver/controller/MainController.java
@@ -3,6 +3,7 @@ package com.example.broadcom.geoserver.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.broadcom.geoserver.dto.SearchQueryDto;
 
@@ -14,6 +15,13 @@ public class MainController {
 
         model.addAttribute("searchInput" , searchInput);
         return "welcome"; //view
+    }
+
+    @PostMapping("/search")
+    public String search(Model model, SearchQueryDto searchInput) {
+
+        model.addAttribute("searchInput", searchInput);
+        return "searchResult"; //view
     }
 
 }
