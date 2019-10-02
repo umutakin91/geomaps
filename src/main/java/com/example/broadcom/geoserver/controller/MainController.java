d45711c53519f68dd143e3053288856762cba0e9
Umut AKIN <umutakin91@gmail.com>
2019-10-02 23:43:38 +0200

    geonames service used in the controller.

--- src/main/java/com/example/broadcom/geoserver/controller/MainController.java
+++ src/main/java/com/example/broadcom/geoserver/controller/MainController.java
@@ -1,14 +1,20 @@
 package com.example.broadcom.geoserver.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.broadcom.geonames.ws.model.SearchResult;
 import com.example.broadcom.geoserver.dto.SearchQueryDto;
+import com.example.broadcom.geoserver.service.GeoNamesService;
 
 @Controller
 public class MainController {
+
+    @Autowired
+    private GeoNamesService geoNamesService;
 
     @GetMapping("/")
     public String main(Model model,SearchQueryDto searchInput) {
@@ -19,8 +25,9 @@ public class MainController {
 
     @PostMapping("/search")
     public String search(Model model, SearchQueryDto searchInput) {
-
+        SearchResult searchResult = geoNamesService.search(searchInput);
         model.addAttribute("searchInput", searchInput);
+        model.addAttribute("searchResult", searchResult);
         return "searchResult"; //view
     }
 
