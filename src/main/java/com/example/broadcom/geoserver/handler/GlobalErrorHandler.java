9d068f88aee62b6d5d229ef456979eaf4cef310e
Umut AKIN <umutakin91@gmail.com>
2019-10-04 22:11:47 +0200

    global error handler introduced.

---
+++ src/main/java/com/example/broadcom/geoserver/handler/GlobalErrorHandler.java
@@ -0,0 +1,26 @@
+package com.example.broadcom.geoserver.handler;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+class GlobalErrorHandler {
+    public static final String DEFAULT_ERROR_VIEW = "error";
+
+    @ExceptionHandler(value = Exception.class)
+    public String defaultErrorHandler(HttpServletRequest req, Exception e, Model model) throws Exception {
+
+        if (AnnotationUtils.findAnnotation
+                (e.getClass(), ResponseStatus.class) != null)
+            throw e;
+
+        model.addAttribute("exception", e);
+        model.addAttribute("url", req.getRequestURL());
+        return "error";
+    }
+}
