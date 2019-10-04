8eee5769d3b24c1305df6799b776f1f991cb669e
Umut AKIN <umutakin91@gmail.com>
2019-10-04 22:09:43 +0200

    GeoConfig updated to use rest client.

--- src/main/java/com/example/broadcom/geoserver/config/GeoConfig.java
+++ src/main/java/com/example/broadcom/geoserver/config/GeoConfig.java
@@ -1,15 +1,36 @@
 package com.example.broadcom.geoserver.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.web.client.ResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
+
+import com.example.broadcom.geoserver.ws.rest.RestClient;
+import com.example.broadcom.geoserver.ws.rest.RestDefaultClientImpl;
 
 @Configuration
 public class GeoConfig {
 
-    @Bean("geonamesRestTemplate")
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.build();
+    @Autowired
+    private ResponseErrorHandler responseErrorHandler;
+
+    @Bean("geonamesRestClient")
+    @Primary
+    public RestClient restClient() {
+        return new RestDefaultClientImpl(getRestTemplate());
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return createRestTemplate();
+    }
+
+    private RestTemplate createRestTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setErrorHandler(responseErrorHandler);
+        return restTemplate;
     }
 }
