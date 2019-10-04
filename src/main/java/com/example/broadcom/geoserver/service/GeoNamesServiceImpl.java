65beabcbc1e3cfb4366dca02ab138aa06e42dab4
Umut AKIN <umutakin91@gmail.com>
2019-10-04 22:17:38 +0200

    service implementation changed to use restclient.

--- src/main/java/com/example/broadcom/geoserver/service/GeoNamesServiceImpl.java
+++ src/main/java/com/example/broadcom/geoserver/service/GeoNamesServiceImpl.java
@@ -2,27 +2,28 @@ package com.example.broadcom.geoserver.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.example.broadcom.geonames.ws.model.SearchResult;
 import com.example.broadcom.geoserver.dto.SearchQueryDto;
+import com.example.broadcom.geoserver.ws.rest.RestClient;
 
 @Service
 public class GeoNamesServiceImpl implements GeoNamesService {
 
     @Autowired
-    @Qualifier(value = "geonamesRestTemplate")
-    private RestTemplate restTemplate;
+    @Qualifier(value = "geonamesRestClient")
+    private RestClient restClient;
 
     @Override
     public SearchResult search(SearchQueryDto searchQueryDto) {
 
-        final String uri = "http://api.geonames.org/searchJSON?formatted=true&q="+ searchQueryDto.getQuery() +"&maxRows=2&lang=es&username=geosetter&style=MEDIUM";
+        final String uri = "http://api.geonames.org/searchJSON?formatted=true&q="+ searchQueryDto.getQ() +"&maxRows=2&lang=es&username=geosetter&style=MEDIUM";
 
-        SearchResult result = restTemplate.getForObject(uri, SearchResult.class);
+        ResponseEntity<? extends SearchResult> result = restClient.get(uri, SearchResult.class);
+        SearchResult searchResult = result.getBody();
 
-        System.out.println(result);
-        return result;
+        return searchResult;
     }
 }
