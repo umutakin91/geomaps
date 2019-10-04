e0cc1f3c0018b8761e516bdd034583c00e7b35d9
Umut AKIN <umutakin91@gmail.com>
2019-10-05 00:44:57 +0200

    search query updated with lang parameter.

--- src/main/java/com/example/broadcom/geoserver/dto/SearchQueryDto.java
+++ src/main/java/com/example/broadcom/geoserver/dto/SearchQueryDto.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 public class SearchQueryDto implements Serializable {
 
     String q;
+    String lang;
 
     public String getQ() {
         return q;
@@ -14,10 +15,19 @@ public class SearchQueryDto implements Serializable {
         this.q = q;
     }
 
+    public String getLang() {
+        return lang;
+    }
+
+    public void setLang(String lang) {
+        this.lang = lang;
+    }
+
     @Override
     public String toString() {
         return "SearchQueryDto{" +
                 "q='" + q + '\'' +
+                ", lang='" + lang + '\'' +
                 '}';
     }
 }
