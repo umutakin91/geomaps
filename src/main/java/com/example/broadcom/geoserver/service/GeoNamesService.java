f5d1cfebbbe853405c24496e0abedadc9c65ca01
Umut AKIN <umutakin91@gmail.com>
2019-10-05 00:44:15 +0200

    language list introduced to geo names service.

--- src/main/java/com/example/broadcom/geoserver/service/GeoNamesService.java
+++ src/main/java/com/example/broadcom/geoserver/service/GeoNamesService.java
@@ -1,9 +1,13 @@
 package com.example.broadcom.geoserver.service;
 
+import java.util.List;
+
 import com.example.broadcom.geonames.ws.model.SearchResult;
 import com.example.broadcom.geoserver.dto.SearchQueryDto;
 
 public interface GeoNamesService {
 
     SearchResult search(SearchQueryDto q);
+
+    List<String> getLanguages();
 }
