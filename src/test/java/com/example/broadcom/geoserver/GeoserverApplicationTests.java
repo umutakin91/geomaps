ccf7ae67d3282dcd2c2afd986569486387085d89
Umut AKIN <umutakin91@gmail.com>
2019-10-05 23:40:20 +0200

    checkstyle bug of test class fixed.

--- src/test/java/com/example/broadcom/geoserver/GeoserverApplicationTests.java
+++ src/test/java/com/example/broadcom/geoserver/GeoserverApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
- * Spring Boot main test class
+ * Spring Boot main test class.
  *
  * @author Umut AKIN
  */
